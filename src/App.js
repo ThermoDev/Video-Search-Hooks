45e3778e79327565c5e4e1427b06f81dcca06376
Michael Pegios <michael.pegios@hotmail.com>
2022-02-11 13:50:05 +1100

    Simple Text Handling
    - Simple SearchBar text handling
    - Installed Axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-function App() {
+import React from 'react';
+import SearchBar from './components/SearchBar';
+import VideoDetail from './components/VideoDetail';
+import VideoItem from './components/VideoItem';
+import VideoList from './components/VideoList';
+
+const App = () => {
   return (
-    <div>Test!</div>
+    <div className="ui container">
+      <SearchBar />
+      <VideoList />
+      <VideoDetail />
+      <VideoItem />
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
+  const [term, setTerm] = useState('');
 
-export default SearchBar
+  useEffect(() => {
+    console.log(term)
+  }, [term])
+
+  return (
+    <div>
+      <form className="ui form">
+        <div className="field">
+          <label>Search for a YouTube video!</label>
+          <div className="ui icon input">
+            <input
+              onChange={(e) => setTerm(e.target.value)}
+              value={term}
+              className="input"
+              type="text"
+              name="search"
+              placeholder="Search..."
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
